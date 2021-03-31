9f0bf6f77838fea4aa0cbff54caa9de4241824d7
Fujiokaryo <f.tooru17@gmail.com>
2021-03-31 16:19:47 +0900

    小分け

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,26 +7,48 @@ public class EnemyController : MonoBehaviour
 
     public int hp = 100;
     public int speed;
+
+    private Animator anime;
     private GameObject target;
     private float targetDistance;
+    private bool attackPlayer = false;
+    private float timer;
+    private float attackTime = 1;
 
 
     public void SetUpEnemy(GameObject target)
     {
         this.target = target;
-
+        anime = GetComponent<Animator>();
         transform.LookAt(target.transform);        
     }
 
     private void Update()
     {
+        if(attackPlayer == true)
+        {
+            EnemyAttack();
+
+            return;
+        }
+
+        
+
         this.gameObject.transform.position = Vector3.MoveTowards(transform.position,new Vector3(target.transform.position.x, 0.1f, target.transform.position.z)
             , 6 * Time.deltaTime);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Damage();
+    private void OnTriggerEnter(Collider other)
+    { 
+    
+            Damage();
+
+            if (other.gameObject.tag == "MainCamera")
+            {      
+
+                ChangeAttackFlag();
+            }
+        
     }
 
     private void Damage()
@@ -41,4 +63,19 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    private void EnemyAttack()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > attackTime)
+        {
+            timer = 0;
+            anime.SetTrigger("Attack");
+        }
+    }
+
+    private void ChangeAttackFlag()
+    {
+        attackPlayer = true;
+    }
 }
